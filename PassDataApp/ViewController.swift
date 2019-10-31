7d82bd7523f21f788a76f73763f4a823724ec456
Yuta Fujii <yutayutakid21@gmail.com>
2019-10-31 18:38:44 +0900

    メモつきです！

--- PassDataApp/ViewController.swift
+++ PassDataApp/ViewController.swift
@@ -6,12 +6,31 @@
 //  Copyright © 2019 Fujii Yuta. All rights reserved.
 //
 
+/*
+ 
+ 近藤さん・・・テキストフィールドを2つ作り、ボタンを押した際に値を渡してください。(ViewController.swift)、
+
+ 中塚さんがpush後に再度クローンし、セグエをつないでください。idはnext
+ 
+ 中塚さん・・・値を受け取りラベルに表示するものを作成してください。(NextViewController.swift)
+ 
+ 
+ 
+ 渡す変数名はname、comment、受け取る変数名もname、comment
+ 
+ 
+ 
+ */
+
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        
         // Do any additional setup after loading the view.
     }
 
